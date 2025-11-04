8d8323e9f9fad7590cbb3d2bbbac0fab7665ec91
LinRuizhou <770117459@qq.com>
2025-11-04 16:11:52 +0800

    5

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import Person from "./components/Person.vue";
 </script>
 
 <template>
+  <Person />
+
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
   </nav>
-
   <RouterView />
 </template>
 
---
+++ src/components/Person.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const name = ref<string>("张三");
+const age = ref<number>(18);
+const tel: string = "13811112222";
+
+function changeName(): void {
+  name.value = "zhang_san";
+}
+
+function changeAge(): void {
+  age.value++;
+}
+
+function showTel(): void {
+  alert(tel);
+}
+</script>
+
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<style scoped>
+.person {
+  background-color: skyblue;
+}
+</style>
