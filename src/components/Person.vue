2a072e9ebc16cc8dbd89e69bbbbd605d3e3911b8
LinRuizhou <770117459@qq.com>
2025-11-05 15:20:54 +0800

    14

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 /* ------------------------------------------------------------------------------------------ */
 
@@ -8,32 +8,21 @@ type Car = {
   price: number;
 };
 
-const benz = ref<Car>({
+const benz: Car = reactive({
   brand: "奔驰",
   price: 100,
 });
 
 function changePrice(): void {
-  benz.value.price += 10;
+  benz.price += 10;
 }
 
 /* ------------------------------------------------------------------------------------------ */
 
-type Game = {
-  id: string;
-  name: string;
-};
+const sum = ref<number>(0);
 
-const games = ref<Array<Game>>([
-  { id: "111", name: "王者" },
-  { id: "222", name: "原神" },
-  { id: "333", name: "三国" },
-]);
-
-function changeFirstName(): void {
-  if (games.value[0]) {
-    games.value[0].name = "魔兽世界";
-  }
+function changeSum(): void {
+  sum.value += 1;
 }
 </script>
 
@@ -41,12 +30,9 @@ function changeFirstName(): void {
   <div class="person">
     <h2>汽车信息：一辆{{ benz.brand }}车，价值{{ benz.price }}万</h2>
     <button @click="changePrice">修改汽车的价格</button>
-    <br />
-    <h2>游戏列表：</h2>
-    <ul>
-      <li v-for="game in games">{{ game.name }}</li>
-    </ul>
-    <button @click="changeFirstName">修改第一个名字</button>
+    <hr />
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="changeSum">点我sum+1</button>
   </div>
 </template>
 
