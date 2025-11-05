bb388989d3371620c2b5a49c4684d32fd97fbab1
LinRuizhou <770117459@qq.com>
2025-11-05 11:30:36 +0800

    12

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,33 +1,52 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive } from "vue";
 
-const name = ref<string>("张三");
-const age = ref<number>(18);
-const tel: string = "13811112222";
-const address: string = "广州市";
+/* ------------------------------------------------------------------------------------------ */
 
-function changeName(): void {
-  name.value = "zhang_san";
+type Car = {
+  brand: string;
+  price: number;
+};
+
+const benz: Car = reactive({
+  brand: "奔驰",
+  price: 100,
+});
+
+function changePrice(): void {
+  benz.price += 10;
 }
 
-function changeAge(): void {
-  age.value++;
-}
+/* ------------------------------------------------------------------------------------------ */
 
-function showTel(): void {
-  alert(tel);
+type Game = {
+  id: string;
+  name: string;
+};
+
+const games: Array<Game> = reactive([
+  { id: "111", name: "王者" },
+  { id: "222", name: "原神" },
+  { id: "333", name: "三国" },
+]);
+
+function changeFirstName(): void {
+  if (games[0]) {
+    games[0].name = "魔兽世界";
+  }
 }
 </script>
 
 <template>
   <div class="person">
-    <h2>姓名：{{ name }}</h2>
-    <h2>年龄：{{ age }}</h2>
-    <h2>地址：{{ address }}</h2>
-    <hr />
-    <button @click="changeName">修改名字</button>
-    <button @click="changeAge">修改年龄</button>
-    <button @click="showTel">查看联系方式</button>
+    <h2>汽车信息：一辆{{ benz.brand }}车，价值{{ benz.price }}万</h2>
+    <button @click="changePrice">修改汽车的价格</button>
+    <br />
+    <h2>游戏列表：</h2>
+    <ul>
+      <li v-for="game in games">{{ game.name }}</li>
+    </ul>
+    <button @click="changeFirstName">修改第一个名字</button>
   </div>
 </template>
 
