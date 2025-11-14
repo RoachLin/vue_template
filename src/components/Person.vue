326321225b28bf0660fd84833c3eac38fe45f889
LinRuizhou <770117459@qq.com>
2025-11-14 15:55:02 +0800

    16.1

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,39 +1,18 @@
 <script setup lang="ts">
-import { reactive, toRefs, toRef, type Ref } from "vue";
+import { ref } from "vue";
 
-/* ------------------------------------------------------------------------------------------ */
-
-type Person = {
-  name: string;
-  age: number;
-};
-
-const person: Person = reactive({
-  name: "张三",
-  age: 18,
-});
-
-const { name: name1, age: age1 } = toRefs(person);
-
-function changeName(): void {
-  name1.value += "~";
-}
-
-function changeAge(): void {
-  age1.value += 1;
-}
-
-/* ------------------------------------------------------------------------------------------ */
-
-const age2: Ref<number> = toRef(person, "age");
+const firstName = ref<string>("");
+const lastName = ref<string>("");
 </script>
 
 <template>
   <div class="person">
-    <h2>姓名：{{ name1 }}</h2>
-    <h2>年龄：{{ age1 }}，{{ age2 }}</h2>
-    <button @click="changeName">修改名字</button>
-    <button @click="changeAge">修改年龄</button>
+    姓：<input type="text" v-model="firstName" />
+    <br />
+    名： <input type="text" v-model="lastName" />
+    <br />
+    全名： <span>{{ firstName }}-{{ lastName }}</span>
+    <br />
   </div>
 </template>
 
