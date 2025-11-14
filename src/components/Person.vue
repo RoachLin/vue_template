43bcc4752056365eb4d060e1763306f42d7518a5
LinRuizhou <770117459@qq.com>
2025-11-14 17:28:15 +0800

    16.3

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -4,9 +4,20 @@ import { computed, ref } from "vue";
 const firstName = ref<string>("");
 const lastName = ref<string>("");
 
-const fullName = computed<string>(() => {
-  return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + "-" + lastName.value;
+const fullName = computed<string>({
+  get: () => {
+    return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + "-" + lastName.value;
+  },
+  set: (newValue: string) => {
+    const [newFirstName = "", newLastName = ""]: string[] = newValue.split("-");
+    firstName.value = newFirstName;
+    lastName.value = newLastName;
+  },
 });
+
+function changeFullName(): void {
+  fullName.value = "li-si";
+}
 </script>
 
 <template>
@@ -16,6 +27,8 @@ const fullName = computed<string>(() => {
     名： <input type="text" v-model="lastName" />
     <br />
     全名： <span>{{ fullName }}</span>
+    <br />
+    <button @click="changeFullName">将全名改为li-si</button>
     <br />
   </div>
 </template>
